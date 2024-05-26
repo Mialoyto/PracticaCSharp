be18084ee975de331652f26e88f28a5286fcb0fa
Miguel Loyola Torres <mialoyto1999@gmail.com>
2024-05-26 04:09:54 -0500

    Acabe de implementar la nueva caracteristica de tal cosa

---
+++ NewFeature/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NewFeature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // esto solo se debe de subir a la rama 'feature'
+        }
+    }
+}
